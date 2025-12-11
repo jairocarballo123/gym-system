b2a99800476fc03e0449310ee4f7179053346d29
Jairo Carballo <jairocarballolopez5@gmail.com>
2025-12-11 17:32:27 -0600

    Actualizo conexi

--- Backend/DB/db.js
+++ Backend/DB/db.js
@@ -1,11 +1,21 @@
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   user: 'SOPORTE',
+//   host: 'localhost',
+//   database: 'BD_SG',
+//   password: '210108',
+//   port: 5432,
+// });
+
+// module.exports = pool;
+
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'SOPORTE',
-  host: 'localhost',
-  database: 'BD_SG',
-  password: '210108',
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
+
