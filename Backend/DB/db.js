fa364d59e163b4a7a0b5fd501854170a7875e3a0
Jairo Carballo <jairocarballolopez5@gmail.com>
2026-02-01 11:04:54 -0600

    correcion cnbd

--- Backend/DB/db.js
+++ Backend/DB/db.js
@@ -1,21 +1,11 @@
-// const { Pool } = require('pg');
-
-// const pool = new Pool({
-//   user: 'SOPORTE',
-//   host: 'localhost',
-//   database: 'BD_SG',
-//   password: '210108',
-//   port: 5432,
-// });
-
-// module.exports = pool;
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
+  user: 'SOPORTE',
+  host: 'localhost',
+  database: 'BD_SG',
+  password: '210108',
+  port: 5432,
 });
 
 module.exports = pool;
-
